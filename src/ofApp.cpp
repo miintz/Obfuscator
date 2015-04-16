877a6786078b859a9e25b526c433e9fe4b569db2
miintz <vanheesmaarten@hotmail.com>
2015-04-16 18:17:31 +0200

    maarten: idk anymore

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -377,7 +377,7 @@ void ofApp::draw(){
 		}
 
 		//draw background
-		cvimg.draw(640, 0, camWidth*SCALE, camHeight*SCALE);	
+		cvimg.draw(0, 0, camWidth*SCALE, camHeight*SCALE);	
 		//draw mirrored background
 		//mirrorTexture.draw(camWidth, 0, camWidth, camHeight);
 
